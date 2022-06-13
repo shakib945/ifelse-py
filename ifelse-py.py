cf284feaff8c84a9b32cb903b92bd7d4aa60fedd
shakib945 <47948456+shakib945@users.noreply.github.com>
2022-06-13 09:30:59 -0400

    Add files via upload

---
+++ ifelse-py.py
@@ -0,0 +1,43 @@
+name = input("Enter your name: ")
+x = int (input  ("Enter your result number "))
+
+
+
+
+if x <= 32 :
+    print("you have a fail "+name)
+
+
+
+elif  x <= 33 :
+    print("you have a pass " +name)
+    
+    
+    
+    
+elif  x <= 40 :
+    print(" result number B " +name)
+ 
+  
+elif  x <= 50 :
+    print(" result number A- "+name)
+    
+elif  x <= 60 :
+    print(" result number A "+name)
+    
+    
+elif  x <= 70 :
+    print(" result number A++ Four "+name)
+    
+elif x <= 80 :
+    print(" result number A++ Three  "+name)
+ 
+elif x <= 90 :
+    print(" result number A++ Two "+name)
+    
+elif x <= 100 :
+    print(" result number A++ One "+name)    
+    
+    
+else:
+   print("you have a fail "+name)
